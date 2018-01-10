3f4398a356b8a67f7ddc20c064e7d38d8b9b55f7
Robert-Yaman <robert.e.yaman@gmail.com>
2018-01-09 16:21:37 -0800

    Keep track of processed sets

--- objects/sequenceSet.py
+++ objects/sequenceSet.py
@@ -8,15 +8,23 @@ class SequenceSet:
 		# Enforce uniqueness on sequences by using a dict.
 		self.frequency_dict = frequency_dict
 
+	def __len__(self):
+		return len(self.frequency_dict)
+
 	def setFrequency(self, sequence, frequency):
 		if not type(sequence) == Seq:
-			print "ERROR: sequence should be Seq object: " + str(type(sequence))
+			print "ERROR: sequence should be Seq object: %s" %(
+				str(type(sequence)))
 			raise Exception
 		self.frequency_dict[str(sequence)] = frequency
+		# Don't store 0s for equality comparisons.
+		if int(frequency) == 0:
+			del self.frequency_dict[str(sequence)]
 
 	def frequency(self, sequence):
 		if not type(sequence) == Seq:
-			print "ERROR: sequence should be Seq object: " + str(type(sequence))
+			print "ERROR: sequence should be Seq object: %s" %(
+				str(type(sequence)))
 			raise Exception
 		# Use str(sequence) for backward compatibility.
 		key = str(sequence)
@@ -26,10 +34,17 @@ class SequenceSet:
 			return self.frequency_dict[key]
 
 	def sequences(self):
-		return [Seq(seq, generic_protein) for seq in self.frequency_dict.keys()]
+		return [Seq(seq, 
+			generic_protein) for seq in self.frequency_dict.keys()]
 
 	def contains(self, seq):
 		return self.frequency(seq) > 0
 
 	def isExactly(self, seq):
 		return len(self.sequences()) == 1 and self.contains(seq)
+
+	def isEqual(self, other_set):
+		"""Efficient equality checker."""
+		# Use internal struct for efficiency here. It's fine since it's an 
+		# internal method.
+		return self.frequency_dict == other_set.frequency_dict
